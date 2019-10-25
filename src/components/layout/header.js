5ecd20f302a14ab94c363ee4f6e327a1edbd0fa3
Chandrakanth Guntupalli <chandrakanth_guntupalli@epam.com>
2019-10-25 17:07:26 +0530

    coded for layout

---
+++ src/components/layout/header.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import CssBaseline from '@material-ui/core/CssBaseline'; 
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+const drawerWidth = 240;
+const useStyles = makeStyles(theme => ({    
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: 36,
+    },
+    hide: {
+      display: 'none',
+    },
+}));
+
+const Header = (props)=> {
+  const classes = useStyles();
+  const theme = useTheme();
+  const { onSidebarOpen, open } = props;
+
+  return (
+    <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={onSidebarOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Mini variant drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    
+  );
+}
+
+export default Header;
+
---
+++ src/components/layout/layout.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Header from './header';
+import Footer from './footer';
+import Content from './content';
+import Sidebar from './sidebar';
+
+const useStyles = makeStyles(theme => ({    
+    root: {
+        display: 'flex',
+    },
+    layout: {
+        backgroundColor: { color: 'yellow' }
+    },
+    sidebar: {
+        float: 'left'
+    }
+}));
+
+export default function Layout() {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+    return (
+        <div className={classes.root}>
+            <Header onSidebarOpen={handleDrawerOpen} open={open} />
+            <Sidebar onSidebarClose={handleDrawerClose} open={open} />
+            <Content />
+            <div>
+                <Footer/>
+            </div>
+            
+            {/* <div>
+                <div className={classes.sidebar}><Sidebar onSidebarClose={handleDrawerClose} open={open} /></div>
+                <div>
+                    <Content/>
+                    <Footer/>
+                </div>
+            </div>             */}
+        </div>
+    )
+}
