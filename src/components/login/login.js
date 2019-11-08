cd54fa0addef8c360395c05f7e5f3f8130830210
chandrakanthg <chandrakanth.mca@gmail.com>
2019-11-08 10:42:21 +0530

    login design

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
 
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
 import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { withStyles } from "@material-ui/core/styles";
 
 const style = {
     margin: 15,
 };
-class Login extends Component {
+const styles = theme=> ({
+    '@global': {
+        body: {
+            backgroundColor: theme.palette.common.white,
+        }
+    },
+    paper:{
+        marginTOp: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+});
+
+class Login extends Component {    
     constructor(props) {
         super(props);
         this.sate = {
@@ -15,28 +50,79 @@ class Login extends Component {
             password: '',
         }
     }
+    
+    handleSignIn(e) {
+        e.preventDefault();
+        let username = e.target.name;
+        console.log('clicked');
+        console.log(e.target);
+    }
 
     render() {
+        const { classes } = this.props;
         return(
-            <div>
-                
-                <TextField hintText="Enter user name" floatingLabelText="username" onChange={(event, newValue)=> {
-                    this.setState({username: newValue})
-                }}/>
-                <TextField hintText="Enter password" floatingLabelText="password" onChange={(event, newValue)=> {
-                    this.setState({password: newValue})
-                }}/>
-                <Button label="Login" style={style} primary={true} onClick={(event)=> {
-                    this.handleClick(event)
-                }} />
-                
-            </div>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />            
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <form className={classes.form} onSubmit= {this.handleSignIn.bind(this)} noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="User name"
+                            name="username"
+                            autoComplete="username"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>                
+                </div>
+            </Container>
         );
     }
 }
-// const Login = ()=> {
-//     console.log('login clicked');
-//     return ('Login comp');
-// }
 
-export default Login;
+export default withStyles(styles)(Login);
