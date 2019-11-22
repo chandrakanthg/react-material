2ec37b6681e2bb038b2a4713a073472433862433
Chandrakanth Guntupalli <chandrakanth_guntupalli@epam.com>
2019-11-22 15:44:19 +0530

    implemented the login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -11,72 +11,95 @@ import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-
-import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import { Formik, Form, Field, ErrorMessage, yupToFormErrors, useField } from 'formik';
+import * as Yup from 'yup';
 
 import { withStyles } from "@material-ui/core/styles";
+import { red } from '@material-ui/core/colors';
 
 const style = {
     margin: 15,
 };
-const styles = theme=> ({
+const styles = theme => ({
     '@global': {
         body: {
             backgroundColor: theme.palette.common.white,
         }
     },
-    paper:{
+    paper: {
         marginTOp: theme.spacing(8),
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
     },
     avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
     },
     form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(1),
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
     },
     submit: {
-      margin: theme.spacing(3, 0, 2),
+        margin: theme.spacing(3, 0, 2),
     },
+    invalid_feedback: {
+        color: red
+    }
 });
 
-class Login extends Component {    
+// class MyCheckBox extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
+//     render() {
+//         const {label, field, meta} = this.props;
+//         return(
+//             <FormControlLabel {...field} control={<Checkbox />} label={label} />
+//         )
+//     }
+// }
+
+const MyCheckBox = ({label, ...props}) => {    
+    const [field, meta] = useField(props);    
+    return(
+        <FormControlLabel {...field} control={<Checkbox />} label={label} />
+    )
+};
+
+class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            username: '',
-            password: '',
-            loginErrors: '',
-            submitted: false,
-        }
-        this.handleSignIn = this.handleSignIn.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+        // this.state = {
+        //     username: '',
+        //     password: '',
+        //     loginErrors: '',
+        //     submitted: false,
+        // }
+        // this.handleSignIn = this.handleSignIn.bind(this);
+        // this.handleChange = this.handleChange.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
-    
-    handleSignIn(e) {
-        e.preventDefault();
-        const  {username, password} = this.state;
-        
-        console.log('clicked');
-        console.log(username, password);
-    }
+    // handleChange(event) {
+    //     this.setState({
+    //         [event.target.name]: event.target.value
+    //     })
+    // }
+
+    // handleSignIn(e) {
+    //     e.preventDefault();
+    //     const { username, password } = this.state;
+
+    //     console.log('clicked');
+    //     console.log(username, password);
+    // }
 
     render() {
         const { classes } = this.props;
-        const { submitted } = this.state;
-        return(
+        // const { submitted } = this.state;
+        return (
             <Container component="main" maxWidth="xs">
-                <CssBaseline />            
+                <CssBaseline />
                 <div className={classes.paper}>
                     <Avatar className={classes.avatar}>
                         <LockOutlinedIcon />
@@ -84,63 +107,59 @@ class Login extends Component {
                     <Typography component="h1" variant="h5">
                         Sign in
                     </Typography>
-                    <ValidatorForm ref="form" className={classes.form} onSubmit= {this.handleSignIn} noValidate>
-                        <TextField
-                            variant="outlined"
-                            margin="normal"                            
-                            fullWidth
-                            id="username"
-                            label="User name"
-                            name="username"
-                            autoComplete="username"
-                            value = {this.state.username}
-                            onChange = {this.handleChange}
-                            validators = {['required', 'isEmail']}
-                            errorMessages = {['field is required', 'email is not valid']}
-                            autoFocus
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                            value = {this.state.password}
-                            onChange = {this.handleChange}
-                            validators = {['required']}
-                            errorMessages = {['field is required']}
-                        />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Remember me"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                            disabled = {submitted}
-                        >
-                            Sign In
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
+                    <Formik
+                        initialValues={{ username: '', password: '', rememberMe: false }}
+                        validationSchema={Yup.object().shape({
+                            username: Yup.string()
+                                .email('Email is invalid')
+                                .required('User name is required'),
+                            password: Yup.string()
+                                .min(6, 'Password must be at least 6 characters')
+                                .required('Email is required')
+                        })}
+                        onSubmit={(fields, { setSubmitting }) => {
+                            setSubmitting(true);
+                            alert('SUCCESS!! :-)\n\n' + JSON.stringify(fields, null, 4))
+                            setSubmitting(false);
+                            // this.handleSignIn()
+                        }}
+                        render={({ values, isSubmitting, touched, errors}) => (
+                            <Form className={classes.form} noValidate>
+                                <Field placeholder="Enter user name" variant="outlined" margin="normal" error={touched.username && Boolean(errors.username)}
+                                    fullWidth name="username" type="input" as={TextField} />
+                                    <ErrorMessage name="username" component="div" />
+                                <Field placeholder="Enter password" variant="outlined" margin="normal" error={touched.password && Boolean(errors.password)}
+                                    fullWidth name="password" type="password" as={TextField} />
+                                    <ErrorMessage name="password" component="div"  />
+                                
+                                <MyCheckBox name="rememberMe" label="Remember me"  type="checkbox"/>
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.submit}
+                                    disabled={isSubmitting}
+                                >
+                                    Sign In
+                                </Button>
+                                <Grid container>
+                                    <Grid item xs>
+                                        <Link href="#" variant="body2">
+                                            Forgot password?
                                 </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="#" variant="body2">
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </ValidatorForm>                
+                                    </Grid>
+                                    <Grid item>
+                                        <Link href="#" variant="body2">
+                                            {"Don't have an account? Sign Up"}
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                                <pre>{JSON.stringify(values, null, 2)}</pre>
+                            </Form>
+                        )}
+                    >
+                    </Formik>
                 </div>
             </Container>
         );
